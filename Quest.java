e8e8440f8771bc6d259895b8c78f5351a54a71b5
TzoNiCeid <64524300+TzoNiCeid@users.noreply.github.com>
2020-05-06 11:53:59 +0300

    Update Quest.java

--- Quest.java
+++ Quest.java
@@ -8,14 +8,16 @@
 public class Quest 
 {
     
-    public int questNumber;
-    public int missioners;
+    privete int questNumber;
+    private int missioners;
+    private Current_Game players_num;
 
     
-    public Quest(int questNumber, int missioners)
+    public Quest(int questNumber, int missioners, Current_Game num)
     {
         this.questNumber=questNumber;
         this.missioners=missioners;
+        players_num=num;
     }
 
    
