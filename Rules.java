967bd4afdf00a9126542823763c2669ea0a92096
paulr21 <64547126+paulr21@users.noreply.github.com>
2020-05-13 18:34:54 +0300

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -15,8 +15,36 @@ public class Rules
 	}
 
     	public boolean verifySpRoles(boolean validSpRoles)
-    	{
-    	 //eksetasi me obj tou chooseSpRoles()
-	return validSpRoles;
-   	}
+    {
+     if(percival.isUsed = true && merlin.isUsed = false)
+     {
+         validSpRoles = false;
+        }
+     
+     else if(mordred.isUsed = true && merlin.isUsed = false)
+     {
+         validSpRoles = false;
+        }
+        
+     else if(morgana.isUsed = true && (merlin.isUsed = false || percival.isUsed = false))
+     {
+         validSpRoles = false;
+        }
+     else if(numPlayers = 5)
+     {
+         if(percival.isUsed = true && mordred.isUsed = false && morgana.isUsed = false)
+         {
+             valiSpRoles = false;
+            }
+         else if( merlin.isUsed = true && percival.isUsed = true && mordred.isUsed = true && oberon.isUsed = true && morgana.isUsed = true)
+         {
+             validSpRoles = false;
+            }  
+     }
+     else {
+         validSpRoles = true;
+        }
+     
+     return validSpRoles;
+    }
 }
