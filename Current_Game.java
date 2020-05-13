cb42b751312674ad1ab2ae27e2ba3ced07cbcff3
IreneGalani <64428000+IreneGalani@users.noreply.github.com>
2020-05-13 20:35:09 +0300

    Update Current_Game.java

--- Current_Game.java
+++ Current_Game.java
@@ -3,18 +3,30 @@ public class Current_Game {
 	
 public int numPlayers; //Metavlhth pou orizei posoi paiktes dhlwthikan gia thn sigkekrimenh partida.
 public int rolesAssigned; // Metavlhth pou orizei ton metrhth pou metraei posoi xarakthres exoun dwthei se paiktes.
-public String [] playerList = new String [10] ; // Pinakas pou kataxwrountai ta onomata pou exoun lavei meros sth sugkekrimenh patida.
-
+public String [] playerList = new String [numPlayers] ; // Pinakas pou kataxwrountai ta onomata pou exoun lavei meros sth sugkekrimenh patida.
+private int keepLeader; //voithitiki metavliti pou krataei ti thesi tou arxhgou sth lista paiktwn
+private VotingData Vdata = new VotingData();
+PlayerLeader theLeader;
+	
 public Current_game (int numPlayers, int rolesAssigned)
 {
 	this.numPlayers = numPlayers;
 	this.rolesAssigned = rolesAssigned;
 	playerList = new String[10] {" "," "," "," "," "," "," "," "," "," "};
+	this.keepLeader = 0;
 	
+}
+
+public void setPlayerList(String[] playerList) {
+        this.playerList = playerList;
 }
 
 public int getnumPlayers(){
 	return numPlayers;
+}
+	
+public void setNumPlayers(int numPlayers) {
+        this.numPlayers = numPlayers;
 }
 
 public int getroleAssigned(){
@@ -29,4 +41,24 @@ public void setrolesAssigned(int rolesA){
 	rolesAssigned = rolesA;
 }
 
+public void VoteFails(){
+        int fails = Vdata.checkFailures();
+        if(fails > 0 && fails < 5) {
+            String leader = chooseLeader();
+	    theLeader = new PlayerLeader(leader);
+            theLeader.setLeader(true);
+	}
+}
+
+//μέθοδος για την επιλογή του παίκτη αρχηγού
+public String chooseLeader(){
+        if(this.keepLeader < this.playerList.length){
+            this.keepLeader++;
+        } else{
+            this.keepLeader = 1;
+        }
+        return this.playerList[keepLeader-1];
+    }
+	
+	
 }
