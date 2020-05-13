916c85129b7209228f15f60d45db00d50b7a25ae
paulr21 <64547126+paulr21@users.noreply.github.com>
2020-05-13 13:31:58 +0300

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -1,7 +1,22 @@
 
-public class Rules {
+public class Rules
+{
 	
+	private boolean validNumPlayers = false;
+	private boolean validSpRoles = false;
 	
-}
+  	public boolean verifyNumPlayers(boolean validNumPlayers)
+	{
+       		if(numPlayers >= 5 && numPlayers <= 10)
+		{
+           		validNumPlayers = true;
+           		return validNumPlayers; 
+		}
+	}
 
+    	public boolean verifySpRoles(boolean validSpRoles)
+    	{
+    	 //eksetasi me obj tou chooseSpRoles()
+	return validSpRoles
+   	}
 }
