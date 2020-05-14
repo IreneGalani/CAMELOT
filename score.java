a7944ba512d6103478ca3d9168358f0545294233
MikeTsakiridis <64594356+MikeTsakiridis@users.noreply.github.com>
2020-05-14 16:25:27 +0300

    Update score.java

--- score.java
+++ score.java
@@ -1,8 +1,9 @@
-
-
 public class score {
     int goodSc;
     int evilSc;
+    private Role role=new Role();
+    private Questoutcome questoutcome=new Questoutcome();
+    private SpecialRules sp=new SpecialRules();
     //constructor
     public score() {
         this.goodSc=0;
@@ -14,4 +15,36 @@ public class score {
     public void setEvilsc(int esc) {
         this.evilSc=esc;
     }
+    public void setRole ( Role role) {
+        this.role=role;
+    }
+    public void setspecialRules(SpecialRules sp){
+        this.sp=sp;
+    }
+    public  int adde() {
+        this.evilSc++;
+        return this.evilSc;
+    }
+     public  int addg() {
+        this.goodSc++;
+        return this.goodSc;
+    }
+    public  void checkscore(){
+    if (this.goodSc==3) {
+        role.checkformerlin();
+    } else if (this.evilSc==3) {
+        System.out.println("EVILS WIN.");
+    }
+}
+public  void scorecheck(){
+    if (this.evilSc<3 && questoutcome.fails>0) {
+    sp.LoLcheck();
+} else if (this.evilSc==3) {
+    questoutcome.checkfinishing();
+} else if (this.goodSc==3) {
+    questoutcome.checkfinishing();
+} else if (this.goodSc<3 && questoutcome.fails==0) {
+    sp.LoLcheck();
+}
+}
 }
