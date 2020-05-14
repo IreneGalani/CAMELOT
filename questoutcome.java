eddcff5838726c73622a1f147c55a3061ec780b0
IreneGalani <64428000+IreneGalani@users.noreply.github.com>
2020-05-14 13:06:00 +0300

    Update questoutcome.java

--- questoutcome.java
+++ questoutcome.java
@@ -1,16 +1,33 @@
 
-public class questoutcome {
+public class QuestOutcome {
    int fails;
    int completedQuests;
    //constructor
-   public questoutcome() {
+   public QuestOutcome() {
        this.fails=0;
        this.completedQuests=0;
     }
    public void setFails(int f) {
-       this.fails=fails;
+       this.fails=f;
     }
     public void setCompletedQuests(int compq) {
         this.completedQuests=compq;
+    }
+   
+   public int checkCompleted(){
+        switch (this.completedQuests){
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            default:
+                return 0;
+        }
     }
 }
