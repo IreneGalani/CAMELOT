80baf68ec96b795c1b3b14392435a8518b1af634
TzoNiCeid <64524300+TzoNiCeid@users.noreply.github.com>
2020-05-16 22:03:23 +0300

    Update History.java

--- History.java
+++ History.java
@@ -1,4 +1,5 @@
 
+
 /**
  *  History
  * 
@@ -10,19 +11,27 @@ public class History{
     private boolean emptyHistory;
     private boolean saveGame;// save current game?
     private int savedGames;// #of saved Games
-    private Player name;// player's name
     
-
-    public History(int savedGames, Player username)
+    //nea
+          
+    private String us;//username   
+    private String winnerside;
+    private boolean newpl;//new player
+    
+    private historyData[] allPlayerList= new historyData[100];
+    private int x=0;// metrhths gia xwro list
+    historyData h;  
+    
+    public History(boolean svG)
     {
         this.emptyHistory=true;
-        this.saveGame=false;
+        this.saveGame=svG;
         this.savedGames=0;
-        username=name;
+        
     }
 
     
-    
+   
     public void setEmptyHistory(boolean emHistory)
     {
         this.emptyHistory= emHistory;
@@ -37,8 +46,55 @@ public class History{
     {
         this.savedGames= savedG;
     }
-}
-   
+    //new! apo dw kai katw
+    public void HistoryExist(boolean emHistory,boolean saveG, int savedG){
+       if (savedGames!=0){
+        this.emptyHistory=false;
+        this.savedGames= savedG+1;
+    }
+    }    
     
+    public void addToHistory(historyData h){
+       if(this.saveGame=true){
+        if(x<allPlayerList.length){                            
+           
+              if(this.newpl==true){
+                  x++;
+                  this.allPlayerList[x]=h;                
+              }
+            
+             for(int i=1; i==x; i++){ 
+               this.allPlayerList[i].setHistoryData(us,winnerside);
+              }               
+           
+        }        
+      }
+    }
+    
+    public void clearHistory(historyData[] allPlayerList){
+        this.emptyHistory=true;        
+        this.savedGames=0;
+        for(int i=1; i==x; i++){                
+               this.allPlayerList[i].setHistoryData("null","null");
+        }
+        this.x=0;  
+        
+    }
+    
+    
+   
+    public void checkIfNewPlayer(boolean NewPlayer ){
+         if(NewPlayer) {
+         this.newpl=true;
+       }
+        
+    }
+    public historyData[] GetList(){
+        return this.allPlayerList;
+    }
+    
+    
+    
+}
     
 
