65c3ac3786eff9af6c27286e5d049d12d871e6a2
MikeTsakiridis <64594356+MikeTsakiridis@users.noreply.github.com>
2020-05-25 13:57:28 +0300

    Update score.java

--- score.java
+++ score.java
@@ -1,11 +1,12 @@
+package camelot;
 
-public class score {
+public class Score {
     int goodSc;
     int evilSc;
-    private Questoutcome questoutcome=new Questoutcome();
+    QuestOutcome questoutcome=new QuestOutcome();
     
     //constructor
-    public score() {
+    public Score() {
         this.goodSc=0;
         this.evilSc=0;
     }
@@ -15,40 +16,52 @@ public class score {
     public void setEvilsc(int esc) {
         this.evilSc=esc;
     }
-    public  int adde() {
-        this.evilSc++;
-        return this.evilSc;
-    }
-     public  int addg() {
-        this.goodSc++;
+    public int getGoodSc() {
         return this.goodSc;
+        
     }
-    public  void checkscore(boolean mer){
-    if (this.goodSc==3 && mer==true) {
-         System.out.println("EVIL's LAST CHANCE");
-    } else if (this.evilSc==3) {
-        System.out.println("EVILS WIN.");
+     public int getEvilSc() {
+        return this.evilSc;
+        
     }
-    else if (this.goodSc==3 && mer==false) {
-    System.out.println("GOOD WIN.");
-}
-}
-public  void scorecheck(boolean lol){
-    if (this.evilSc<3 && questoutcome.fails>0) {
-        if (lol==true){
-             System.out.println("LADY OF THE LAKE");
-            } else {
-             System.out.println("NEW LEADER WANTED");
-            };
-    } else if (this.evilSc==3) {
-      questoutcome.checkcompleted();
-    } else if (this.goodSc==3) {
-      questoutcome.checkcompleted();
-    } else if ((this.goodSc<3) && (questoutcome.fails==0)) {
-        if (lol==true){
-          System.out.println("LADY OF THE LAKE");
-        } else
-          System.out.println("NEW LEADER WANTED");
-        };
-    }
+     
+     public int adde () {
+    	 this.goodSc++;
+    	 return goodSc;
+     }
+     
+     public int addg() {
+    	evilSc++;
+    	return evilSc;
+     }
+     
+     public  void checkscore(boolean mer){
+    	    if (this.goodSc==3 && mer==true) {
+    	         System.out.println("EVIL's LAST CHANCE");
+    	    } else if (this.evilSc==3) {
+    	        System.out.println("EVILS WIN.");
+    	    }
+    	    else if (this.goodSc==3 && mer==false) {
+    	    System.out.println("GOOD WIN.");
+    	}
+    	}
+    	public  void scorecheck(boolean lol){
+    	    if (this.evilSc<3 && questoutcome.fails>0) {
+    	        if (lol==true){
+    	             System.out.println("LADY OF THE LAKE");
+    	            } else {
+    	             System.out.println("NEW LEADER WANTED");
+    	            };
+    	    } else if (this.evilSc==3) {
+    	      questoutcome.checkCompleted();
+    	    } else if (this.goodSc==3) {
+    	      questoutcome.checkCompleted();
+    	    } else if ((this.goodSc<3) && (questoutcome.fails==0)) {
+    	        if (lol==true){
+    	          System.out.println("LADY OF THE LAKE");
+    	        } else
+    	          System.out.println("NEW LEADER WANTED");
+    	        };
+    	    }
+
 }
